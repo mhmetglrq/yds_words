2c259fde1551e189c1afe815774703916ad85303
Mehmet Guler <mhmetglr.q@gmail.com>
2025-03-16 01:43:43 +0300

    Add WordEntity and WordModel classes

---
+++ lib/features/ydsWords/data/models/word_model.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'word_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class WordModelAdapter extends TypeAdapter<WordModel> {
+  @override
+  final int typeId = 2;
+
+  @override
+  WordModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return WordModel(
+      id: fields[0] as String,
+      word: fields[1] as String,
+      translation: fields[2] as String,
+      type: fields[3] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, WordModel obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.word)
+      ..writeByte(2)
+      ..write(obj.translation)
+      ..writeByte(3)
+      ..write(obj.type);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is WordModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
