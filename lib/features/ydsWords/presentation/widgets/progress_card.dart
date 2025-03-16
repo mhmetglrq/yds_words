3be58ce08936350a4e4eb09282e89ae88ee411f1
Mehmet Guler <mhmetglr.q@gmail.com>
2025-03-16 04:15:58 +0300

    Add word learning bloc and progress indicator

--- lib/features/ydsWords/presentation/widgets/progress_card.dart
+++ lib/features/ydsWords/presentation/widgets/progress_card.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:yds_words/config/extensions/context_extension.dart';
 
 import '../../../../config/items/colors/app_colors.dart';
+import '../blocs/wordLearning/word_learning_bloc.dart';
 
 class ProgressCard extends StatelessWidget {
   const ProgressCard({
@@ -19,64 +21,80 @@ class ProgressCard extends StatelessWidget {
         elevation: 5,
         child: Padding(
           padding: context.paddingLow,
-          child: Column(
-            children: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "İlerleyiş",
-                  style: context.textTheme.headlineLarge,
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              //Buraya bir progress bar ekleyelim
-              Padding(
-                padding: context.paddingVerticalLow,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(8), // yuvarlak köşeler
-                  child: LinearProgressIndicator(
-                    minHeight: 8,
-                    value: 0.5, // %50 dolu
-                    backgroundColor: AppColors.kInfoColor,
-                    valueColor: AlwaysStoppedAnimation<Color>(
-                      AppColors.kPrimaryColor,
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: context.paddingVerticalLow,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: BlocBuilder<WordLearningBloc, WordLearningState>(
+            builder: (context, state) {
+              if (state is WordLearningLoading ||
+                  state is WordLearningInitial) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (state is WordLearningLoaded) {
+                return Column(
                   children: [
-                    Text(
-                      "Öğrenilen Kelime",
-                      style: context.textTheme.bodyMedium,
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "İlerleyiş",
+                        style: context.textTheme.headlineLarge,
+                        textAlign: TextAlign.center,
+                      ),
                     ),
-                    Text(
-                      "50",
-                      style: context.textTheme.bodyMedium,
+                    //Buraya bir progress bar ekleyelim
+                    Padding(
+                      padding: context.paddingVerticalLow,
+                      child: ClipRRect(
+                        borderRadius:
+                            BorderRadius.circular(8), // yuvarlak köşeler
+                        child: LinearProgressIndicator(
+                          minHeight: 8,
+                          value: 0.5, // %50 dolu
+                          backgroundColor: AppColors.kInfoColor,
+                          valueColor: AlwaysStoppedAnimation<Color>(
+                            AppColors.kPrimaryColor,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: context.paddingVerticalLow,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Öğrenilen Kelime",
+                            style: context.textTheme.bodyMedium,
+                          ),
+                          Text(
+                            "${state.learnedWords.length}",
+                            style: context.textTheme.bodyMedium,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: context.paddingVerticalLow,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Kalan Kelime",
+                            style: context.textTheme.bodyMedium,
+                          ),
+                          Text(
+                            "${state.words.length}",
+                            style: context.textTheme.bodyMedium,
+                          ),
+                        ],
+                      ),
                     ),
                   ],
-                ),
-              ),
-              Padding(
-                padding: context.paddingVerticalLow,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Toplam Kelime",
-                      style: context.textTheme.bodyMedium,
-                    ),
-                    Text(
-                      "100",
-                      style: context.textTheme.bodyMedium,
-                    ),
-                  ],
-                ),
-              ),
-            ],
+                );
+              } else {
+                return Center(
+                    child:
+                        Text("Error: ${(state as WordLearningError).message}"));
+              }
+            },
           ),
         ),
       ),
