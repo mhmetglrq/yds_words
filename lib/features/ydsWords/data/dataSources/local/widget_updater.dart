805d66e61ff55b93594b9cea9a4b4fc09446f675
Mehmet Guler <mhmetglr.q@gmail.com>
2025-03-19 23:59:30 +0300

    Add WidgetUpdater class for updating widget with random word

---
+++ lib/features/ydsWords/data/dataSources/local/widget_updater.dart
@@ -0,0 +1,40 @@
+import 'package:home_widget/home_widget.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:yds_words/core/resources/data_state.dart';
+import 'package:yds_words/features/ydsWords/domain/entities/word_entity.dart';
+import 'package:yds_words/features/ydsWords/domain/repositories/word_learning_repository.dart';
+
+class WidgetUpdater {
+  final WordLearningRepository _wordLearningRepository;
+
+  WidgetUpdater(this._wordLearningRepository);
+
+  Future<void> updateWidget() async {
+    final DataState<WordEntity> result =
+        await _wordLearningRepository.getRandomWordForWidget();
+    if (result is DataSuccess) {
+      final wordEntity = result.data!;
+      print(
+          "Gönderilen kelime: ${wordEntity.word}, Anlam: ${wordEntity.translation}");
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      // Veri yazımını kontrol et
+      final wordSaved =
+          // await HomeWidget.saveWidgetData<String>('word_text', wordEntity.word);
+          await prefs.setString("word_text", wordEntity.word);
+      final meaningSaved =
+          //  await HomeWidget.saveWidgetData<String>(
+          //     'meaning_text', wordEntity.translation);
+          await prefs.setString("meaning_text", wordEntity.translation);
+      print(
+          "Kelime kaydedildi mi? $wordSaved, Anlam kaydedildi mi? $meaningSaved");
+
+      final updateSuccess = await HomeWidget.updateWidget(
+        name: 'WordWidgetProvider',
+        iOSName: 'WordWidget',
+      );
+      print("Widget güncelleme başarılı mı? $updateSuccess");
+    } else {
+      print("Hata: ${result.message}");
+    }
+  }
+}
